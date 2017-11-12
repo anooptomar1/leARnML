02737c64917d7a77a3f705aafed1489f2284b090
cvxluo <cvxluo@users.noreply.github.com>
2017-11-12 06:14:35 -0500

    Inital Files.

---
+++ leARn/leARn/ViewController+Actions.swift
@@ -0,0 +1,151 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+UI Actions for the main view controller.
+*/
+
+import UIKit
+import SceneKit
+
+extension ViewController: UIGestureRecognizerDelegate {
+    
+    enum SegueIdentifier: String {
+        case showObjects
+    }
+    
+    // MARK: - Interface Actions
+    
+    /// Displays the `VirtualObjectSelectionViewController` from the `addObjectButton` or in response to a tap gesture in the `sceneView`.
+    @IBAction func showVirtualObjectSelectionViewController() {
+        // Ensure adding objects is an available action and we are not loading another object (to avoid concurrent modifications of the scene).
+        guard !addObjectButton.isHidden && !virtualObjectLoader.isLoading else { return }
+        
+        statusViewController.cancelScheduledMessage(for: .contentPlacement)
+        performSegue(withIdentifier: SegueIdentifier.showObjects.rawValue, sender: addObjectButton)
+        
+        //isHidden/isEnabled Code
+        periodicAddObject.isHidden = true
+        periodicAddObject.isEnabled = false
+        
+    }
+    
+    @IBAction func showPeriodicTable() {
+        guard !addObjectButton.isHidden && !virtualObjectLoader.isLoading else { return }
+        
+        statusViewController.cancelScheduledMessage(for: .contentPlacement)
+        performSegue(withIdentifier: SegueIdentifier.showObjects.rawValue, sender: periodicAddObject)
+        
+        //isHidden/isEnabled Code
+        addObjectButton.isHidden = true
+        addObjectButton.isEnabled = false
+        elementTextField.isHidden = false
+        elementTextField.isEnabled = true
+        elementSendButton.isHidden = false
+        elementSendButton.isEnabled = true
+        
+    }
+        
+    @IBAction func showElement() {
+        //guard !addObjectButton.isHidden && !virtualObjectLoader.isLoading else { return }
+        
+        statusViewController.cancelScheduledMessage(for: .contentPlacement)
+        virtualObjectLoader.removeAllVirtualObjects()
+        var object = VirtualObject()
+        for virtualElementObject in VirtualObject.elements {
+            if virtualElementObject.modelName == elementDictionary[elementTextField.text!] {
+                object = virtualElementObject
+            }
+        }
+        virtualObjectLoader.loadVirtualObject(object, loadedHandler: { [unowned self] loadedObject in
+            DispatchQueue.main.async {
+                self.hideObjectLoadingUI()
+                self.placeVirtualObject(loadedObject)
+            }
+        })
+        elementTextField.text! = ""
+       
+        //isHidden/isEnabled Code
+        periodicAddObject.isHidden = true
+        periodicAddObject.isEnabled = false
+        
+    }
+    
+    /// Determines if the tap gesture for presenting the `VirtualObjectSelectionViewController` should be used.
+    func gestureRecognizerShouldBegin(_: UIGestureRecognizer) -> Bool {
+        return virtualObjectLoader.loadedObjects.isEmpty
+    }
+    
+    func gestureRecognizer(_: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith _: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    
+    /// - Tag: restartExperience
+    func restartExperience() {
+        guard isRestartAvailable, !virtualObjectLoader.isLoading else { return }
+        isRestartAvailable = false
+
+        statusViewController.cancelAllScheduledMessages()
+
+        virtualObjectLoader.removeAllVirtualObjects()
+        addObjectButton.setImage(UIImage(named: "ellipsis"), for: [])
+        addObjectButton.setImage(UIImage(named: "ellipsisPressed"), for: [.highlighted])
+        periodicAddObject.setImage(UIImage(named: "pImage"), for: [])
+        periodicAddObject.setImage(UIImage(named: "pImagePressed"), for: [.highlighted])
+
+        resetTracking()
+        
+        //isHidden/isEnabled Code
+        addObjectButton.isHidden = false
+        addObjectButton.isEnabled = true
+        periodicAddObject.isHidden = false
+        periodicAddObject.isEnabled = true
+        elementTextField.isHidden = true
+        elementTextField.isEnabled = false
+        elementSendButton.isHidden = true
+        elementSendButton.isEnabled = false
+
+        // Disable restart for a while in order to give the session time to restart.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+            self.isRestartAvailable = true
+        }
+    }
+}
+
+extension ViewController: UIPopoverPresentationControllerDelegate {
+    
+    // MARK: - UIPopoverPresentationControllerDelegate
+
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+        return .none
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // All menus should be popovers (even on iPhone).
+        if let popoverController = segue.destination.popoverPresentationController, let button = sender as? UIButton {
+            popoverController.delegate = self
+            popoverController.sourceView = button
+            popoverController.sourceRect = button.bounds
+        }
+        
+        guard let identifier = segue.identifier,
+              let segueIdentifer = SegueIdentifier(rawValue: identifier),
+              segueIdentifer == .showObjects else { return }
+        
+        let objectsViewController = segue.destination as! VirtualObjectSelectionViewController
+        if periodicAddObject == (sender as! UIButton) {
+            objectsViewController.virtualObjects = VirtualObject.periodicTables
+        }
+        else {
+            objectsViewController.virtualObjects = VirtualObject.misc
+        }
+        objectsViewController.delegate = self
+        
+        // Set all rows of currently placed objects to selected.
+        for object in virtualObjectLoader.loadedObjects {
+            guard let index = VirtualObject.availableObjects.index(of: object) else { continue }
+            objectsViewController.selectedVirtualObjectRows.insert(index)
+        }
+    }
+    
+}
---
+++ leARn/leARn/ViewController.swift
@@ -0,0 +1,264 @@
+//
+//  ViewController.swift
+//  leARn
+//
+//  Created by Charlie Luo on 11/11/17.
+//
+
+import ARKit
+import SceneKit
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: IBOutlets
+    
+    @IBOutlet var sceneView: VirtualObjectARView!
+    
+    @IBOutlet weak var addObjectButton: UIButton!
+    
+    @IBOutlet weak var blurView: UIVisualEffectView!
+    
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
+    // MARK: - Custom Elements
+    
+    @IBOutlet weak var periodicSpinner: UIActivityIndicatorView!
+    
+    @IBOutlet weak var periodicAddObject: UIButton!
+    
+    @IBOutlet weak var scaleSlider: UISlider! {
+        didSet {
+            scaleSlider.transform = CGAffineTransform(rotationAngle: CGFloat(-Double.pi/2))
+            
+            scaleSlider.minimumValue = 0.0
+            scaleSlider.maximumValue = 1.5
+            scaleSlider.value = 0.5
+        }
+    }
+    
+    @IBAction func scaleSliderChanged(_ sender: Any) {
+        let newScale = pow(scaleSlider.value, 2) + 0.2
+        for object in virtualObjectLoader.loadedObjects {
+            object.scale = SCNVector3(newScale, newScale, newScale)
+        }
+        
+    }
+    
+    @IBOutlet weak var elementTextField: UITextField!
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Dismisses the Keyboard by making the text field resign
+        // first responder
+        textField.resignFirstResponder()
+        
+        // returns false. Instead of adding a line break, the text
+        // field resigns
+        return false
+    }
+    
+    @IBOutlet weak var elementSendButton: UIButton!
+    
+    let elementDictionary = [
+        "H" : "hydrogen",
+        "He" : "helium",
+        "Li" : "lithium",
+        "Be" : "beryllium",
+        "B" : "boron",
+        "C" : "carbon",
+        "N" : "nitrogen",
+        "O" : "oxygen",
+        "F" : "fluorine",
+        "Fe" : "iron",
+        "Cu" : "copper",
+        "Au" : "gold"
+        
+    ]
+    
+    // MARK: - UI Elements
+    
+    var focusSquare = FocusSquare()
+    
+    /// The view controller that displays the status and "restart experience" UI.
+    lazy var statusViewController: StatusViewController = {
+        return childViewControllers.lazy.flatMap({ $0 as? StatusViewController }).first!
+    }()
+    
+    // MARK: - ARKit Configuration Properties
+    
+    /// A type which manages gesture manipulation of virtual content in the scene.
+    lazy var virtualObjectInteraction = VirtualObjectInteraction(sceneView: sceneView)
+    
+    /// Coordinates the loading and unloading of reference nodes for virtual objects.
+    let virtualObjectLoader = VirtualObjectLoader()
+    
+    /// Marks if the AR experience is available for restart.
+    var isRestartAvailable = true
+    
+    /// A serial queue used to coordinate adding or removing nodes from the scene.
+    let updateQueue = DispatchQueue(label: "com.example.apple-samplecode.arkitexample.serialSceneKitQueue")
+    
+    var screenCenter: CGPoint {
+        let bounds = sceneView.bounds
+        return CGPoint(x: bounds.midX, y: bounds.midY)
+    }
+    
+    /// Convenience accessor for the session owned by ARSCNView.
+    var session: ARSession {
+        return sceneView.session
+    }
+    
+    // MARK: - View Controller Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sceneView.delegate = self
+        sceneView.session.delegate = self
+        elementTextField.delegate = self
+        
+        // Set up scene content.
+        setupCamera()
+        sceneView.scene.rootNode.addChildNode(focusSquare)
+        
+        //isHidden/isEnabled Code
+        elementTextField.isHidden = true
+        elementTextField.isEnabled = false
+        elementSendButton.isHidden = true
+        elementSendButton.isEnabled = false
+        
+        
+        /*
+         The `sceneView.automaticallyUpdatesLighting` option creates an
+         ambient light source and modulates its intensity. This sample app
+         instead modulates a global lighting environment map for use with
+         physically based materials, so disable automatic lighting.
+         */
+        sceneView.automaticallyUpdatesLighting = false
+        if let environmentMap = UIImage(named: "art.scnassets/sharedImages/environment_blur.exr") {
+            sceneView.scene.lightingEnvironment.contents = environmentMap
+        }
+        
+        // Hook up status view controller callback(s).
+        statusViewController.restartExperienceHandler = { [unowned self] in
+            self.restartExperience()
+        }
+        /*
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(showVirtualObjectSelectionViewController))
+        // Set the delegate to ensure this gesture is only used when there are no virtual objects in the scene.
+        tapGesture.delegate = self
+        sceneView.addGestureRecognizer(tapGesture)
+        
+        
+        let periodicTapGesture = UITapGestureRecognizer(target: self, action: #selector(showPeriodicTable))
+        
+        periodicTapGesture.delegate = self
+        sceneView.addGestureRecognizer(periodicTapGesture)
+        
+        let elementTapGesture = UITapGestureRecognizer(target: self, action: #selector(showElement))
+        
+        elementTapGesture.delegate = self
+        sceneView.addGestureRecognizer(elementTapGesture) */
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Prevent the screen from being dimmed to avoid interuppting the AR experience.
+        UIApplication.shared.isIdleTimerDisabled = true
+        
+        // Start the `ARSession`.
+        resetTracking()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        session.pause()
+    }
+    
+    // MARK: - Scene content setup
+    
+    func setupCamera() {
+        guard let camera = sceneView.pointOfView?.camera else {
+            fatalError("Expected a valid `pointOfView` from the scene.")
+        }
+        
+        /*
+         Enable HDR camera settings for the most realistic appearance
+         with environmental lighting and physically based materials.
+         */
+        camera.wantsHDR = true
+        camera.exposureOffset = -1
+        camera.minimumExposure = -1
+        camera.maximumExposure = 3
+    }
+    
+    // MARK: - Session management
+    
+    /// Creates a new AR configuration to run on the `session`.
+    func resetTracking() {
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+        
+        statusViewController.scheduleMessage("FIND A SURFACE TO PLACE AN OBJECT", inSeconds: 7.5, messageType: .planeEstimation)
+    }
+    
+    // MARK: - Focus Square
+    
+    func updateFocusSquare() {
+        let isObjectVisible = virtualObjectLoader.loadedObjects.contains { object in
+            return sceneView.isNode(object, insideFrustumOf: sceneView.pointOfView!)
+        }
+        
+        if isObjectVisible {
+            focusSquare.hide()
+        } else {
+            focusSquare.unhide()
+            statusViewController.scheduleMessage("TRY MOVING LEFT OR RIGHT", inSeconds: 5.0, messageType: .focusSquare)
+        }
+        
+        // We should always have a valid world position unless the sceen is just being initialized.
+        guard let (worldPosition, planeAnchor, _) = sceneView.worldPosition(fromScreenPosition: screenCenter, objectPosition: focusSquare.lastPosition) else {
+            updateQueue.async {
+                self.focusSquare.state = .initializing
+                self.sceneView.pointOfView?.addChildNode(self.focusSquare)
+            }
+            
+            return
+        }
+        
+        updateQueue.async {
+            self.sceneView.scene.rootNode.addChildNode(self.focusSquare)
+            let camera = self.session.currentFrame?.camera
+            
+            if let planeAnchor = planeAnchor {
+                self.focusSquare.state = .planeDetected(anchorPosition: worldPosition, planeAnchor: planeAnchor, camera: camera)
+            } else {
+                self.focusSquare.state = .featuresDetected(anchorPosition: worldPosition, camera: camera)
+            }
+        }
+
+        statusViewController.cancelScheduledMessage(for: .focusSquare)
+    }
+    
+    // MARK: - Error handling
+    
+    func displayErrorMessage(title: String, message: String) {
+        // Blur the background.
+        blurView.isHidden = false
+        
+        // Present an alert informing about the error that has occurred.
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let restartAction = UIAlertAction(title: "Restart Session", style: .default) { _ in
+            alertController.dismiss(animated: true, completion: nil)
+            self.blurView.isHidden = true
+            self.resetTracking()
+        }
+        alertController.addAction(restartAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+}
+
